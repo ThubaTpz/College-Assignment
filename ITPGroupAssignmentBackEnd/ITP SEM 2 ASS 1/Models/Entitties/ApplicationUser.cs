e6f37171c595113d547e513ff9ea21e472365ead
SiphokaziSibisi <treasuresibisi5@gmail.com>
2025-09-29 18:47:55 +0200

    Update ApplicationUser.cs

--- ITPGroupAssignmentBackEnd/ITP SEM 2 ASS 1/Models/Entitties/ApplicationUser.cs
+++ ITPGroupAssignmentBackEnd/ITP SEM 2 ASS 1/Models/Entitties/ApplicationUser.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
-
 using Microsoft.AspNetCore.Identity;
+
+
 
 namespace ITP_SEM_2_ASS_1.Models.Entitties
 {
     public class ApplicationUser : IdentityUser <int>// Identify if user exists login
     {
+
         public  Guid ApplicationUserId { get;set; }
-        public required string Email {  get; set; }
-        public required string Password { get; set; }
-        public required string PasswordHash {get;set; }
+        //public required string Email {  get; set; }
+        //public required string Password { get; set; }
+        //public required string PasswordHash {get;set; }
 
         public Student StudentProfile { get; set; }
         public Lecturer LecturerProfile { get; set; }
@@ -17,5 +18,9 @@ namespace ITP_SEM_2_ASS_1.Models.Entitties
         public Guid ApplicationRoleId { get; set; } //FK of the role
         public ApplicationRole Role { get; set; } // navigation property to represent the manager
         //relationship 1 to many ( 1 role belong to many users , a user can belong to 1 role)
+
+       
+
+
     }
 }
