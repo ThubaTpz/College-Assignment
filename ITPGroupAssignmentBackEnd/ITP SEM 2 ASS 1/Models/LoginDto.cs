05e12fd990e2de5648512a2d6508cf20758b10e7
SiphokaziSibisi <treasuresibisi5@gmail.com>
2025-09-29 18:51:55 +0200

    adding login nd register dto

---
+++ ITPGroupAssignmentBackEnd/ITP SEM 2 ASS 1/Models/LoginDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ITP_SEM_2_ASS_1.Models
+{
+    public class LoginDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+        
+    }
+}
---
+++ ITPGroupAssignmentBackEnd/ITP SEM 2 ASS 1/Models/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ITP_SEM_2_ASS_1.Models
+{
+    public class RegisterDto
+    {
+        public required string Email{ get; set; }
+        public required string Password{ get; set; }
+        public required string Role{ get; set; }
+    }
+}
