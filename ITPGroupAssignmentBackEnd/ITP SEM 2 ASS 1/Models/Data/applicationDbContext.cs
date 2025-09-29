3b6d12087226bbd948058fc8fc4460acda76de54
SiphokaziSibisi <treasuresibisi5@gmail.com>
2025-09-29 18:46:07 +0200

    Update applicationDbContext.cs

--- ITPGroupAssignmentBackEnd/ITP SEM 2 ASS 1/Models/Data/applicationDbContext.cs
+++ ITPGroupAssignmentBackEnd/ITP SEM 2 ASS 1/Models/Data/applicationDbContext.cs
@@ -1,14 +1,15 @@
-﻿using ITP_SEM_2_ASS_1.Models.Entitties;
+using ITP_SEM_2_ASS_1.Models.Entitties;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
- 
+using Microsoft.Identity.Client;
+
 namespace ITP_SEM_2_ASS_1.Models.Data
 {
     public class applicationDbContext :IdentityDbContext<ApplicationUser,ApplicationRole,int>
     {
         public applicationDbContext(DbContextOptions<applicationDbContext> options) : base(options)
         {
-
+            
         }
 
         public DbSet<Admin> Admins { get; set; }
